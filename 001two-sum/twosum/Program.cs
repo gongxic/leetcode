ebc6e069b70f5c9433186699bdd9feefad1bc5df
Xicheng <gongxicheng1111@hotmail.com>
2021-02-10 18:04:22 +0800

    update

--- 001two-sum/twosum/Program.cs
+++ 001two-sum/twosum/Program.cs
@@ -9,4 +9,20 @@ namespace twosum
             Console.WriteLine("Hello World!");
         }
     }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { i, dic[target - nums[i]] };
+                }
+                dic[nums[i]] = i;
+            }
+            return null;
+        }
+    }
 }
--- 002add-two-numbers/addtwonumbers/Program.cs
+++ 002add-two-numbers/addtwonumbers/Program.cs
@@ -9,100 +9,97 @@ namespace addtwonumbers
             Console.WriteLine("Hello World!");
         }
     }
-}
 
-
-
-// Definition for singly-linked list.
-public class ListNode
-{
-    public int val;
-    public ListNode next;
-    public ListNode(int val = 0, ListNode next = null)
+    // Definition for singly-linked list.
+    public class ListNode
     {
-        this.val = val;
-        this.next = next;
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
     }
-}
 
 
 
-public class Solution
-{
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    public class Solution
     {
-        var newListNode = new ListNode();
-        if (l1 != null && l2 != null)
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            newListNode.val = l1.val + l2.val;
-            if (newListNode.val >= 10)
+            var newListNode = new ListNode();
+            if (l1 != null && l2 != null)
             {
-                newListNode.val = newListNode.val - 10;
-                if (l1.next == null)
+                newListNode.val = l1.val + l2.val;
+                if (newListNode.val >= 10)
                 {
-                    l1.next = new ListNode { val = 1 };
+                    newListNode.val = newListNode.val - 10;
+                    if (l1.next == null)
+                    {
+                        l1.next = new ListNode { val = 1 };
+                    }
+                    else
+                    {
+                        l1.next.val = l1.next.val + 1;
+                    }
+                    newListNode.next = AddTwoNumbers(l1.next, l2.next);
                 }
                 else
                 {
-                    l1.next.val = l1.next.val + 1;
+                    newListNode.next = AddTwoNumbers(l1.next, l2.next);
                 }
-
-                newListNode.next = AddTwoNumbers(l1.next, l2.next);
             }
-            else
+            else if (l1 != null)
             {
-                newListNode.next = AddTwoNumbers(l1.next, l2.next);
-            }
-
-        }
-        else if (l1 != null)
-        {
-            if (l1.val >= 10)
-            {
-                newListNode.val = l1.val - 10;
-                if (l1.next == null)
+                if (l1.val >= 10)
                 {
-                    l1.next = new ListNode { val = 1 };
+                    newListNode.val = l1.val - 10;
+                    if (l1.next == null)
+                    {
+                        l1.next = new ListNode { val = 1 };
+                    }
+                    else
+                    {
+                        l1.next.val = l1.next.val + 1;
+                    }
                 }
                 else
                 {
-                    l1.next.val = l1.next.val + 1;
+                    newListNode.val = l1.val;
                 }
+                newListNode.next = AddTwoNumbers(l1.next, null);
             }
-            else
+            else if (l2 != null)
             {
-                newListNode.val = l1.val;
-            }
-
-            newListNode.next = AddTwoNumbers(l1.next, null);
-        }
-        else if (l2 != null)
-        {
-            if (l2.val >= 10)
-            {
-                newListNode.val = l2.val - 10;
-                if (l1.next == null)
+                if (l2.val >= 10)
                 {
-                    l2.next = new ListNode { val = 1 };
+                    newListNode.val = l2.val - 10;
+                    if (l1.next == null)
+                    {
+                        l2.next = new ListNode { val = 1 };
+                    }
+                    else
+                    {
+                        l2.next.val = l2.next.val + 1;
+                    }
                 }
                 else
                 {
-                    l2.next.val = l2.next.val + 1;
+                    newListNode.val = l2.val;
                 }
-
-            }
-            else
-            {
                 newListNode.val = l2.val;
+                newListNode.next = AddTwoNumbers(l2.next, null);
             }
-            newListNode.val = l2.val;
-            newListNode.next = AddTwoNumbers(l2.next, null);
+            else if (l1 == null && l2 == null)
+            {
+                return l1;
+            }
+            return newListNode;
         }
-        else if (l1 == null && l2 == null)
-        {
-            return l1;
-        }
-
-        return newListNode;
     }
 }
+
+
+
+
