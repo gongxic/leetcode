f8ab7bd75f4ceec4005f425487e0a948abd52211
Xicheng <gongxicheng1111@hotmail.com>
2021-02-10 16:11:18 +0800

    add

---
+++ 001two-sum/two-sum.go
@@ -0,0 +1,24 @@
+package twosum
+
+func twoSum(nums []int, target int) []int {
+	for i, num := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if num+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func twoSum2(nums []int, target int) []int {
+	mapDic := map[int]int{}
+	for i, num := range nums {
+		if x, ok := mapDic[target-num]; ok {
+			return []int{i, x}
+		}
+		mapDic[num] = i
+
+	}
+	return nil
+}
---
+++ 002add-two-numbers/add-two-numbers.cs
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        var newListNode = new ListNode();
+        if(l1!=null&& l2!=null)
+        {
+            newListNode.val= l1.val+l2.val;
+            if(newListNode.val>=10)
+            {
+            newListNode.val=newListNode.val-10;
+            if(l1.next==null)
+            {
+                l1.next = new ListNode{val=1};
+            }
+            else
+            {
+                l1.next.val= l1.next.val+1;
+            }
+            
+            newListNode.next = AddTwoNumbers(l1.next,l2.next);
+        }
+        else
+        {
+            newListNode.next = AddTwoNumbers(l1.next,l2.next);
+        }
+ 
+        }
+        else if(l1!=null)
+        {
+            if(l1.val>=10)
+            {
+                newListNode.val=l1.val-10;
+                if(l1.next==null)
+                {
+                    l1.next = new ListNode{val=1};
+                }else{
+                    l1.next.val= l1.next.val+1;
+                }
+            }else{
+                newListNode.val = l1.val;
+            }
+            
+            newListNode.next = AddTwoNumbers(l1.next,null);
+        }
+       else if(l2!=null)
+        {
+            if(l2.val>=10)
+            {
+                newListNode.val=l2.val-10;
+                if(l1.next==null)
+                {
+                    l2.next = new ListNode{val=1};
+                }
+                else
+                {
+                    l2.next.val= l2.next.val+1;
+                }
+           
+            }else{
+                newListNode.val = l2.val;
+            }
+             newListNode.val = l2.val;
+             newListNode.next = AddTwoNumbers(l2.next,null);
+        } 
+        else if(l1==null&& l2==null)
+        {
+            return l1;
+        }
+        
+        return newListNode;
+    }
+}
